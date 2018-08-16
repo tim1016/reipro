8827144055980e078a616573838c4104964a28aa
tim1016 <inkant@gmail.com>
2018-08-15 19:03:27 -0500

    First commit

---
+++ ReiPro.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name
+ *
+ * @package     REIPro
+ * @author      Inkant Awasthi
+ * @copyright   2016 Inkant Awasthi, Resonance Realty Management
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: REIPro
+ * Plugin URI:  https://example.com/plugin-name
+ * Description: Description of the plugin.
+ * Version:     1.0.0
+ * Author:      Inkant Awasthi
+ * Author URI:  https://example.com
+ * Text Domain: REIPro
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
